ffb6b1cb60525af359413da7da13f0dfb7b786b9
jwcliffe87-arch <jwcliffe87@gmail.com>
2026-04-16 19:58:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker installed");
+const CACHE_NAME = "tracker-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "https://i.postimg.cc/Kjhsytbk/logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", (e) => {
-  // just pass through
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
